b33ffaea297b290e628651675c862d53c222901a
itsnwa <nichlas@itsnwa.com>
2016-06-19 20:59:42 -0400

    Added javascript to remove p tag from images on the project pages.

--- js/nwa.js
+++ js/nwa.js
@@ -5,4 +5,8 @@ $(document).ready(function() {
     $('p.category', $(this)).slideToggle(100, 'linear').display(100, 'linear');
   });
 
+  // Unwrap images from p tags
+  $('.project-content p > img').unwrap().removeAttr('height').removeAttr('width');
+  $('.project-content p > a > img').removeAttr('height').removeAttr('width').parent().unwrap();
+
 });
