943fbbee75a3b41ad75c76a7240279129dd3e2f6
itsnwa <nichlas@itsnwa.com>
2016-06-19 23:39:48 -0400

    Added access token and user ID to nwa.js

--- js/nwa.js
+++ js/nwa.js
@@ -9,4 +9,13 @@ $(document).ready(function() {
   $('.project-content p > img').unwrap().removeAttr('height').removeAttr('width');
   $('.project-content p > a > img').removeAttr('height').removeAttr('width').parent().unwrap();
 
+  var feed = new Instafeed({
+    get: 'user',
+    userId: '781603',
+    accessToken: '781603.1677ed0.b925883d446a42378ec690d6dc250db0',
+    template: '<a href="{{link}}"><img src="{{image}}" /></a>',
+    limit: 12
+  });
+  feed.run();
+
 });
