e6c101e28ebb6028df0011489236d623f2f23c8f
itsnwa <nichlas@itsnwa.com>
2016-07-08 21:35:53 -0400

    Old site

--- gulp/tasks/uploading.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const gulp = require('gulp');
-const ghpages = require('gulp-gh-pages');
-
-// 'gulp deploy' -- pushes your dist folder to Github
-gulp.task('upload', () => {
-  return gulp.src('dist/**/*')
-    .pipe($.ghPages());
-});
